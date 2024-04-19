1e7cb8a7040ecb3058f1878bd40b1dcae1815f2f
EGARRISXN <egarrisxn@gmail.com>
2024-04-18 21:25:43 -0400

    redo of project from bootcamp 1 year ago.

---
+++ README.md
@@ -0,0 +1,49 @@
+# README.md Generator
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+A command-line application that dynamically generates a `README.md` file.
+
+## Table of Contents
+
+- [Technologies](#technologies)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Technologies
+
+Javascript. Inquirer.
+
+## Installation
+
+Clone the repository and run `npm install`.
+
+## Usage
+
+Run `node index.js` and answer the prompts to generate a `README.md` file.
+
+## Tests
+
+This project includes a basic test script in the `package.json` file. The script currently echoes an error message if no tests are specified.
+
+## Contributing
+
+Fork the repository, make changes, and submit a pull request.
+
+## Acnowledgements
+
+Shout out to all my classmates in the UCF Full-Stack Web Development Bootcamp!
+
+## License
+
+This project is licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Questions
+
+Feel free to reach out to me on [GitHub](https://github.com/EGARRISXN) or via email at egarrisxn@gmail.com if you have any questions.
---
+++ main/index.js
@@ -0,0 +1,98 @@
+// Required Packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./renderLicenseBadge");
+
+// Array of prompts for user
+const questions = [
+  {
+    type: "input",
+    message: "Please provide a title for your project:",
+    name: "title",
+    validate: (value) =>
+      value ? true : "Please enter a title for your project.",
+  },
+  {
+    type: "input",
+    message: "Please provide a brief description of your project:",
+    name: "description",
+    validate: (value) =>
+      value ? true : "Please enter a description for your project.",
+  },
+  {
+    type: "input",
+    message: "What technologies were used in your project?",
+    name: "technologies",
+  },
+  {
+    type: "input",
+    message: "What are the steps required to install your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please provide usage instructions/examples:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide examples on how to run tests for your application:",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "How can others contribute to your project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Please provide any acknowledgements or credits:",
+    name: "acknowledgements",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your project:",
+    name: "license",
+    choices: [
+      "MIT",
+      "GNU",
+      "Apache",
+      "BSD",
+      "Mozilla",
+      "Creative Commons",
+      "The Unlicense",
+      "Eclipse",
+    ],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+    validate: (value) => (value ? true : "Please enter your GitHub username."),
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    validate: (value) => (value ? true : "Please enter your email address."),
+  },
+];
+
+// Writes README file
+function writeToFile(data) {
+  if (!fs.existsSync("../output")) fs.mkdirSync("../output");
+  fs.writeFile("../output/README.md", data, (error) => {
+    if (error) console.error("Error writing README file:", error);
+    else console.log("README file created successfully!");
+  });
+}
+
+// Initializes application
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((data) => writeToFile(generateMarkdown(data)))
+    .catch((error) => console.error("Error occurred while prompting:", error));
+}
+
+init();
